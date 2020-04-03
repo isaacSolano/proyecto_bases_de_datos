67737a75470400b20a2061bdf22fee33a8445fd0
Isaac Solano <isolanol@ucenfotec.ac.cr>
2020-04-03 00:18:03 -0600

    Inserts
    Inserts y modificacion de modelo

---
+++ scripts/checktables.sql
@@ -0,0 +1,14 @@
+    select idusuario from usuarios id
+union all
+    select iddirector from directores id
+union all 
+    select idproductor from productores id
+union all
+    select idserie from series id
+union all
+    select idmusico from musicos id
+union all
+    select idcancion from canciones id
+union all
+    select idpelicula from peliculas id
+/
